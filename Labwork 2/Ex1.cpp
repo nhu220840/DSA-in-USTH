cc8f2a67be6124705b5c3ccb92fbcc3f70290194
nhu220840 <nhu220840@gmail.com>
2024-02-07 14:36:16 +0700

    update file ex1 in labwork 2 - fixing

--- Labwork 2/Ex1.cpp
+++ Labwork 2/Ex1.cpp
@@ -4,13 +4,14 @@
 #include <math.h>
 
 typedef struct unboundedInt{
-    int digit;
+    int sign, digit;
     unboundedInt *next;
 } unboundedInt;
 
-unboundedInt* makeDigit(int num){
+unboundedInt* makeDigit(int sign, int num){
     unboundedInt *newNode = (unboundedInt *)malloc(sizeof(unboundedInt));
     newNode->digit = num;
+    newNode->sign = sign;
     newNode->next = NULL;
     return newNode;
 }
@@ -24,13 +25,24 @@ int countDigit(unboundedInt *head){
     return res;
 }
 
+void pushFront(unboundedInt *&head, int num){
+    unboundedInt *newDigit = makeDigit(1, num);
+    newDigit->next = head;
+    head = newDigit;
+}
+
 void addDigit(unboundedInt *&head, int num, int pos){
-    if(pos < 1 || pos > countDigit(head) + 1) return;
-    unboundedInt *newNode = makeDigit(num);
-    if(pos == 1){
-        newNode->next = head;
-        head = newNode;
+    if(head == NULL){
+        printf("ERROR: List is empty\n");
         return;
+    }
+    if(pos < 1 || pos > countDigit(head) + 1){
+        printf("ERROR: The position is out of bound!\n");
+        return;
+    }
+    unboundedInt *newNode = makeDigit(1, num);
+    if(pos == 1){
+        pushFront(head, num);
     }
     unboundedInt *tmp = head;
     for(int i = 1; i <= pos - 2; i++){
@@ -41,15 +53,22 @@ void addDigit(unboundedInt *&head, int num, int pos){
     printf("Added successfully!\n");
 }
 
-void removemDigit(unboundedInt *&head, int num, int pos){
-    if(pos < 1 || pos > countDigit(head) || head == NULL) return;
-    unboundedInt *tmp = head;
+void removemDigit(unboundedInt *&head, int pos){
+    if(head == NULL){
+        printf("ERROR: List is empty!\n");
+        return;
+    }
+    if(pos < 1 || pos > countDigit(head)){
+        printf("ERROR: The postition is out of bound!\n");
+        return;
+    }
     if(pos == 1){
         unboundedInt *del = head; //luu lai node dau tien vao bien del
         head = head->next; //cho head = node thu 2 trong dslk
         free(del); //giai phong node dau tien
         return;
     }
+    unboundedInt *tmp = head;
     for(int i = 1; i <= pos - 2; i++){
         tmp = tmp->next; //duyet den vi tri truoc node can xoa 
     }
@@ -66,9 +85,22 @@ void removemDigit(unboundedInt *&head, int num, int pos){
     printf("Deleted successfully!\n");
 }
 
-int sumOfAllDigits(unboundedInt *head){
+int sumDigits(unboundedInt *head){
     int sum = 0;
+    while(head->next != NULL){
+        sum += head->digit;
+        head = head->next;
+    }
+    return sum;
+}
 
+void display(unboundedInt *head){
+    if(head->sign == -1) printf("-");
+    while(head->next != NULL){
+        printf("%d", head->digit);
+        head = head->next;
+    }
+    printf("\n");
 }
 
 
@@ -76,30 +108,39 @@ int main(){
     unboundedInt *head = NULL;
     printf("Init the sign of unbounded integer (1 is positive and -1 is negative): ");
     int sign; scanf("%d", &sign);
-    
+    head = makeDigit(sign, 1);
     while(true){
         printf("---------------------------------\n");
         printf("1. Add new digit\n");
         printf("2. Remove a digit\n");
         printf("3. Calculate the sum of all digits\n");
         printf("4. Display the whole number\n");
-        printf("0. End\n");
+        printf("0. Exit\n");
         printf("---------------------------------\n");
         
         printf("Enter your choice: ");
         int choice; scanf("%d", &choice);
         if(choice == 1){
-            printf("Enter the value: ");
+            printf("Enter a digit: ");
             int value; scanf("%d", &value);
             printf("Enter the position to add: ");
             int pos; scanf("%d", &pos);
+            addDigit(head, value, pos);
         }
         else if(choice == 2){
             printf("Enter the position to remove: ");
             int pos; scanf("%d", &pos);
+            removemDigit(head, pos);
         }
         else if(choice == 3){
-
+            printf("Sum of all digits is: %d\n", sumDigits(head));
+        }
+        else if(choice == 4){
+            display(head);
+        }
+        else{
+            return 0;
         }
     }
+    return 0;
 }
