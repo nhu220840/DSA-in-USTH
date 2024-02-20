5244e46b304da5b461f126f6ad0a91b12102feb8
nhu220840 <nhu220840@gmail.com>
2024-02-20 16:05:26 +0700

    update file ex1 in labwork 3

--- Labwork 3/Ex1.cpp
+++ Labwork 3/Ex1.cpp
@@ -19,18 +19,73 @@ Product *makeProduct(char *name, int quantity, int price){
     return newProduct;
 }
 
-void addProduct(Product *head, char *name, int quantity, int price){
+void addProduct(Product *&head, char *name, int quantity, int price){
     Product *newProduct = (Product *)malloc(sizeof(Product));
 
     if(head == NULL){
         head = newProduct;
         return;
     }
-    while(head == NULL){
-        head = head->next;
+
+    Product *tmp = head;
+    while(tmp->next == NULL){
+        tmp = tmp->next;
     }
-    head->next = newProduct;
-    newProduct->prev = head;
+    tmp->next = newProduct;
+    newProduct->prev = tmp;
+}
+
+// int validProductName(Product *head, char *name){
+//     Product *tmp = head;
+//     while(tmp != NULL){
+//         if(tmp->name == name){
+//             return 1;
+//         }
+//         tmp = tmp->next;
+//     }
+//     return 0;
+// }
+
+int validProductQuantity(Product *head, char *name, int quantity){
+    Product *tmp = head;
+    
+    while(tmp != NULL){
+        if(tmp->name){
+            if(tmp->quantity >= quantity) return 1;
+            else return 0;
+        }
+        tmp = tmp->next;
+    }
+    return 0;
+}
+
+int getProductQuantity(Product *head, char *name){
+    Product *tmp = head;
+
+    while(tmp != NULL){
+        if(tmp->name == name){
+            return tmp->quantity;
+        }
+        tmp = tmp->next;
+    }
+    return 0;
+}
+
+void reStock(Product *&head, char *name, int quantity, int price){
+    //The product is already in the stock, add more quantity
+    if(getProductQuantity(head, name) != 0){
+        Product *tmp = head;
+        while(tmp != NULL){
+            if(tmp->name == name){
+                tmp->quantity += quantity;
+                break;
+            }
+            tmp = tmp->next;
+        }
+    }
+    else{
+        addProduct(head, name, quantity, price);
+    }
 }
 
 int main(){
