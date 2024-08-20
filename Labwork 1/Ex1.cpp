07f148c4938821face72f01f47558664feccbbda
nhu220840 <nhu220840@gmail.com>
2024-08-20 17:13:48 +0700

    update

--- Labwork 1/Ex1.cpp
+++ Labwork 1/Ex1.cpp
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+// Pseudocode:
+// Input: a positive integer
+// Output: an integer after swapping the first and last digits
+// Process:
+// BEIGN
+//     Get num (char array)
+//     Set n <- length of num
+//     swap(num[0], num[n - 1])
+//     print num
+// END
+
 void swappedNumber(char num[], int n){
     char tmp = num[0];
     num[0] = num[n - 1];
--- algo.cpp
+++ algo.cpp
@@ -1,30 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void bubbleSort(int a[], int n){
-    int isSorted = 0;
-    
-    while(!isSorted){
-        isSorted = 1;
-        for(int i = 0; i < n - 1; i++){
-            if(a[i] > a[i + 1]){
-                int tmp = a[i];
-                a[i] = a[i + 1];
-                a[i + 1] = tmp;
+// Node structure for the linked list
+struct Node {
+    int data;
+    struct Node* next;
+};
 
-                isSorted = 0;
-            }
+// Function to insert a node at the beginning of the linked list
+void push(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+// Function to print the linked list
+void printList(struct Node *node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+// Function to insert a new node in sorted order
+void sortedInsert(struct Node** head_ref, struct Node* new_node) {
+    struct Node* current;
+    if (*head_ref == NULL || (*head_ref)->data >= new_node->data) {
+        new_node->next = *head_ref;
+        *head_ref = new_node;
+    }
+    else {
+        current = *head_ref;
+        while (current->next != NULL && current->next->data < new_node->data) {
+            current = current->next;
         }
+        new_node->next = current->next;
+        current->next = new_node;
     }
 }
 
-int main(){
-    int n; scanf("%d", &n);
-    int a[n];
-    for(int i = 0; i < n; i++){
-        scanf("%d", &a[i]);
+// Function to sort the linked list using insertion sort
+void insertionSort(struct Node **head_ref) {
+    struct Node *sorted = NULL; // Initialize a sorted list
+    struct Node *current = *head_ref; // Traverse the given linked list
+    while (current != NULL) {
+        struct Node *next = current->next; // Store next for next iteration
+        sortedInsert(&sorted, current);
+        current = next;
     }
-    bubbleSort(a, n);
-    for(int i = 0; i < n; i++){
-        printf("%d ", a[i]);
-    }
+    *head_ref = sorted;
+}
+
+int main() {
+    struct Node* a = NULL;
+
+    // Inserting elements into the linked list
+    push(&a, 5);
+    push(&a, 20);
+    push(&a, 4);
+    push(&a, 3);
+    push(&a, 30);
+
+    printf("Linked list before sorting:\n");
+    printList(a);
+
+    // Sorting the linked list using insertion sort
+    insertionSort(&a);
+
+    printf("Linked list after sorting:\n");
+    printList(a);
+
+    return 0;
 }
