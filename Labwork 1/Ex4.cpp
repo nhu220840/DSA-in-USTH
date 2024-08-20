d6dd33d05df0b27ce99668300131570047a189d8
nhu220840 <nhu220840@gmail.com>
2024-08-20 17:24:56 +0700

    update

--- Labwork 1/Ex4.cpp
+++ Labwork 1/Ex4.cpp
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
+//Pseudocode
+// def isSphenic(n):
+//     Set cnt_prime = 0
+//     for i <- 2 to sqrt(n) do:
+//         if n % i == 0:
+//             Set pow = 0
+//             while n % i == 0 do:
+//                 pow = pow + 1
+//                 cnt_prime = cnt_prime + 1
+//                 n = n / i
+//             endwhile
+//             if pow > 1: return False
+//     endfor
+//     if n != 1: cnt_prime = cnt_prime + 1
+    
+//     if cnt_prime == 3: return True
+//     return False
+
 int isSphenic(int n){
     int cnt_prime = 0;
     for(int i = 2; i <= sqrt(n); i++){
